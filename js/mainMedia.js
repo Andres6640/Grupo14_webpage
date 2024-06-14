f4fb21b216a86b4a65811052718b5c4e6912fd61
anteojito <fernando_velo@yahoo.com.ar>
2024-06-14 13:16:25 -0300

    Actualizacion Galeria

--- js/mainMedia.js
+++ js/mainMedia.js
@@ -2,19 +2,53 @@
 
 /* Main de media.html ------------------------ */
 
-const mainMediaJS = `
-    <h1>Galería</h1>
+const images = [{"src": "img/pantera-L-lateral.jpg", "title": "De Tomaso Pantera L"},
+                {"src": "img/pantera-GTS-frente.jpg", "title": "De Tomaso Pantera GTS"},
+                {"src": "img/pantera-Si-frente.jpg", "title": "De Tomaso Pantera 90 Si"},
+                {"src": "img/pantera-L-atras.jpg", "title": "De Tomaso Pantera L"},
+                {"src": "img/pantera-GTS-atras.jpg", "title": "De Tomaso Pantera GTS"},
+                {"src": "img/pantera-Si-atras.jpg", "title": "De Tomaso Pantera 90 Si"}
+];
 
-    <div class="grid-container">
-        <div><img src="img/pantera-L-lateral.jpg" title="De Tomaso Pantera L" alt="De Tomaso Pantera L"></div>
-        <div><img src="img/pantera-GTS-frente.jpg" title="De Tomaso Pantera GTS" alt="De Tomaso Pantera GTS"></div>
-        <div><img src="img/pantera-Si-frente.jpg" title="De Tomaso Pantera 90 Si" alt="De Tomaso Pantera 90 Si"></div>
-        <div><img src="img/pantera-L-atras.jpg" title="De Tomaso Pantera L" alt="De Tomaso Pantera L"></div>
-        <div><img src="img/pantera-GTS-atras.jpg" title="De Tomaso Pantera GTS" alt="De Tomaso Pantera GTS"></div>
-        <div><img src="img/pantera-Si-atras.jpg" title="De Tomaso Pantera 90 Si" alt="De Tomaso Pantera 90 Si"></div>
-        <div><iframe src="https://www.youtube.com/embed/jy_TnTq7NEU?si=3ZAZNipRq-zigOxZ" allowfullscreen></iframe></div>
-        <div><iframe src="https://www.youtube.com/embed/ofFtQOt_xC4?si=sYOdx5QJb8HGTGW-" allowfullscreen></iframe></div>
-        <div><iframe src="https://www.youtube.com/embed/VrkFwBOR444?si=qXp920CQOMgkebFs" allowfullscreen></iframe></div>
-    </div>`;
+const videos = [{"src": "https://www.youtube.com/embed/jy_TnTq7NEU?si=3ZAZNipRq-zigOxZ"},
+                {"src": "https://www.youtube.com/embed/ofFtQOt_xC4?si=sYOdx5QJb8HGTGW-"},
+                {"src": "https://www.youtube.com/embed/VrkFwBOR444?si=qXp920CQOMgkebFs"}
+];
 
-document.querySelector("#mainMedia").innerHTML = mainMediaJS;
+const page = document.getElementById("mainMedia");
+
+/* Crea y agrega <h1>Galería</h1> */
+const pageTitle = document.createElement("h1");
+pageTitle.innerHTML = "Galería";
+page.appendChild(pageTitle);
+
+/* Crea y agrega contenedor de cada imagen/video */
+const container = document.createElement("div");
+container.classList.add("grid-container");
+container.innerHTML = "";
+page.appendChild(container);
+
+/* Crea y agrega cada imagen */
+images.forEach(image => {
+    const frameImage = document.createElement("div");
+    frameImage.innerHTML = `
+        <img src="${image.src}" title="${image.title}" alt="${image.title}">
+    `;
+
+    container.appendChild(frameImage);
+
+    /* Evento click para abrir imagen ampliada */
+    frameImage.addEventListener("click", () => {
+        console.log(image);
+    });
+});
+
+/* Crea y agrega cada video */
+videos.forEach(video => {
+    const frameVideo = document.createElement("div");
+    frameVideo.innerHTML = `
+        <iframe src="${video.src}" allowfullscreen></iframe>
+    `;
+
+    container.appendChild(frameVideo);
+});
