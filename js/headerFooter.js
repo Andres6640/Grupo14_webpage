60aaa1c99bb4ade4cb609f1220371d52af0e8b54
anteojito <fernando_velo@yahoo.com.ar>
2024-06-15 15:24:20 -0300

    Uniforma nombres de archivo

--- js/headerFooter.js
+++ js/headerFooter.js
@@ -1,4 +1,4 @@
-/* include.js */
+/* headerFooter.js */
 
 const headerJS = `
     <div id="topbar">
@@ -21,16 +21,16 @@ document.querySelector("header").innerHTML = headerJS;
 
 const footerJS = `
     <div>
+        <a class="iconoFooter" href="https://www.youtube.com/results?search_query=de+tomaso+pantera" title="Pantera en YouTube" target="_blank">
+            <i class="fa-brands fa-youtube"></i>
+        </a>
+
         <a class="iconoFooter" href="https://www.tiktok.com/search?lang=es&q=de%20tomaso%20pantera&t=1718223652162" title="Pantera en TikTok" target="_blank">
             <i class="fa-brands fa-tiktok"></i>
         </a>
 
         <a class="iconoFooter" href="https://ar.pinterest.com/search/pins/?q=de%20tomaso%20pantera&rs=typed" title="Pantera en Pinterest" target="_blank">
             <i class="fa-brands fa-pinterest"></i>
-        </a>
-
-        <a class="iconoFooter" href="https://www.youtube.com/results?search_query=de+tomaso+pantera" title="Pantera en YouTube" target="_blank">
-            <i class="fa-brands fa-youtube"></i>
         </a>
 
         <a class="iconoFooter" href="contacto.html" title="Contacto" target="">
